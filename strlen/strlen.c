0bfaaf25f284e35f845f1a9911691a4be7f0e4c4
xxj <xxj@192.168.1.4>
2021-01-23 21:14:30 +0800

    增加strlen.c文件

---
+++ strlen/strlen.c
@@ -0,0 +1,13 @@
+#include <strio.h>
+
+size_t
+strlen(char *string){
+	int length = 0;
+	/*
+ 	**依次访问字符串的内容，计数字符数，直到遇见NULL终止符。
+	*/
+    while( *string++ != '\0')	
+        length += 1;
+
+	return length;
+}
